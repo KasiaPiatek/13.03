c0002ffd2da313bf9b265ccf4cf4fb9ff4fe6c16
Kasia Piatek <piatekkatarzynaolga@gmail.com>
2018-01-21 20:21:20 +0100

    add program.js

---
+++ program.js
@@ -0,0 +1,20 @@
+process.stdin.setEncoding('uft-8');
+
+process.stdin.on('readable', function() {
+	// tutaj treść tego co ma się wykonac w moemncie odczytania wejscia.
+	//metoda .read() ma za zadanie odczytac co uzyt
+	//podal na wejsciu
+	var input = process.stdin.read();
+	if(input !==null) {
+		var instruction = input.toString().trim();
+		if( instruction === '/exit') {
+			process.stdout.write('Quitting app!\n');
+			process.exit();
+		} else {
+			process.stderr.write('Wrong instruction!\n');
+		}
+	}
+	console.log (process.versions);
+});
+
+
