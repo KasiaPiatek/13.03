4106fa4ba9166a616a37f9ed09bdf7c2970a198b
Kasia Piatek <piatekkatarzynaolga@gmail.com>
2018-01-24 22:57:58 +0100

    add program.js

--- program.js
+++ program.js
@@ -1,4 +1,4 @@
-process.stdin.setEncoding('utf-8');
+/*process.stdin.setEncoding('utf-8');
 
 
 process.stdin.on('readable', function() {
@@ -18,5 +18,32 @@ process.stdin.on('readable', function() {
 	console.log (process.versions);
 	console.log (process.env)
 });
+*/
+
+process.stdin.setEncoding('utf-8');
+
+
+process.stdin.on('readable', function() {
+
+var input = process.stdin.read();
+if (input !==null) {
+var instruction = input.toString().trim();
+switch(instruction) {
+	case '/exit':
+		process.stdout.write('Quitting app!\n');
+		process.exit();
+	break;
+	case '/lang':
+		process.stderr.write('Language\n');
+		console.log(process.env.lang);
+	break;
+	case '/version.node':
+		process.stdout.write('Node Current Version\n');
+		console.log(process.versions.node);
+	break;
+	default:
+		process.stderr.write('Wrong instruction!\n');
+}}}
+);
 
 
